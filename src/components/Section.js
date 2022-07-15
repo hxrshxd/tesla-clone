728361f22da3755694d81b21a8378d5459f510aa
Harshad <rharshad26@gmail.com>
2022-07-15 14:57:39 +0530

    added more sections, main header, image, buttons, arrow down

---
+++ src/components/Section.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import styled from "styled-components"
+
+function Section() {
+  return (
+    <Wrap>
+      <ItemText>
+        <h1>Model S</h1>
+        <p>Order online touchless delivery</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>Custom order</LeftButton>
+          <RightButton>Existing Inventory</RightButton>
+        </ButtonGroup>
+        <ArrowDown src="/images/down-arrow.svg" />
+      </Buttons>
+    </Wrap>
+  )
+}
+
+export default Section
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-image: url('/images/model-s.jpg');
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width: 786px) {
+    flex-direction: column;
+  }
+`
+
+const LeftButton = styled.div`
+  background-color: rgba(26, 36, 23, 0.8);
+  color: white;
+  width: 256px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 12px;
+  cursor: pointer;
+  height: 40px;
+  border-radius: 100px;
+  text-transform: uppercase;
+  opacity: 0.85;
+  margin: 8px;
+`
+
+const RightButton = styled(LeftButton)`
+  color: black;
+  background: white;
+  opacity: 0.65
+`
+
+const ArrowDown = styled.img`
+  overflow-x: hiddern;
+  height: 30px;
+  animation: animateDown infinite 1.5s
+`
+
+const Buttons = styled.div``
+
+
+
+
+
+
+
+
+
+
