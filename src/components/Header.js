454efcf871c31d8be1a1180eb217324b288a634d
Harshad <rharshad26@gmail.com>
2022-07-17 19:21:45 +0530

    DONE: burger (side) nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,12 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
+import { selectCars } from '../features/car/carSlice';
+import { useSelector } from 'react-redux';
 
 function Header() {
   const [burgerStatus, setBurgerStatus] = useState(false);
+  const cars = useSelector(selectCars);
 
   return (
     <Container>
@@ -12,10 +15,9 @@ function Header() {
         <img src='/images/logo.svg' />
       </a>
       <Menu>
-        <a href='#'>Model S</a>
-        <a href='#'>Model X</a>
-        <a href='#'>Model Y</a>
-        <a href='#'>Model 3</a>
+        { cars && cars.map((car, index) => (
+          <a key={index} href='#'>{ car }</a>
+        )) }
       </Menu>
       <RightMenu>
         <a href='#'>Shop</a>
@@ -26,6 +28,9 @@ function Header() {
         <CloseWrapper>
           <CustomClose onClick={() => setBurgerStatus(false)}/>
         </CloseWrapper>
+        { cars && cars.map((car, index) => (
+          <li><a key={index} href='#'>{ car }</a></li>
+        )) }
         <li><a href='#'>Existing Inventory</a></li>
         <li><a href='#'>Existing Inventory</a></li>
         <li><a href='#'>Existing Inventory</a></li>
