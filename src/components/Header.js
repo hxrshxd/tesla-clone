7c26afd893c1e8679ac75345576cf5a8e2125a00
Harshad <rharshad26@gmail.com>
2022-07-17 18:34:31 +0530

    Added burgernav to show when clicked

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 
 function Header() {
+  const [burgerStatus, setBurgerStatus] = useState(false);
+
   return (
     <Container>
       <a href='#'>
@@ -18,11 +20,11 @@ function Header() {
       <RightMenu>
         <a href='#'>Shop</a>
         <a href='#'>Tesla Account</a>
-        <CustomMenu />
+        <CustomMenu onClick={() => setBurgerStatus(true)}/>
       </RightMenu>
-      <BurgerNav>
+      <BurgerNav show={burgerStatus}>
         <CloseWrapper>
-          <CustomClose />
+          <CustomClose onClick={() => setBurgerStatus(false)}/>
         </CloseWrapper>
         <li><a href='#'>Existing Inventory</a></li>
         <li><a href='#'>Existing Inventory</a></li>
@@ -97,6 +99,8 @@ const BurgerNav = styled.div`
   flex-direction: column;
   text-align: left;
   padding: 20px;
+  transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+  transition: transform 0.2s;
   li {
     padding: 15px 0;
     border-bottom: 1px solid rgba(0, 0, 0, 0.2);
